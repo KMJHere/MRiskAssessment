4a67a5100ef167a37788438983a82d288bbbb825
kmj57 <kmj57@kmj>
2022-10-21 12:59:26 +0900

    [2022-10-21 김민정] USER 정보 공통 클래스 구현

---
+++ src/main/java/com/kmj/safe/common/UserInfo.java
@@ -0,0 +1,64 @@
+package com.kmj.safe.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component
+public class UserInfo {
+	private String COMPANY_ID;
+	
+	private String USER_NO;
+	
+	private String USER_ID;
+	
+	private String USER_NM;
+	
+	public Map<String, Object> getFixData() {
+		Map<String, Object> mData = new HashMap<>();
+		
+		mData.put("COMPANY_ID", this.getCOMPANY_ID());
+		mData.put("USER_NO", this.getUSER_NO());
+		mData.put("USER_ID", this.getUSER_ID());
+		mData.put("USER_NM", this.getUSER_NM());
+		
+		return mData;
+	}
+	
+	public void setSessionData(HttpSession session) {
+		this.setCOMPANY_ID((String)session.getAttribute("COMPANY_ID"));
+		this.setUSER_NO((String)session.getAttribute("USER_NO"));
+		this.setUSER_ID((String)session.getAttribute("USER_ID"));
+		this.setUSER_NM((String)session.getAttribute("USER_NM"));
+	}
+	
+	public void init() {
+		this.setCOMPANY_ID(null);
+		this.setUSER_NO(null);
+		this.setUSER_ID(null);
+		this.setUSER_NM(null);
+	}
+	
+	public Map<String, Object> toMapping(Map<String, Object> amData) {
+		amData.put("COMPANY_ID", this.getCOMPANY_ID());
+		amData.put("USER_NO", this.getUSER_NO());
+		amData.put("USER_ID", this.getUSER_ID());
+		amData.put("USER_NM", this.getUSER_NM());
+		
+		return amData;
+	}
+	
+}
